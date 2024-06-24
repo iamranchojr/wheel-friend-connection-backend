83c3d0b91ca79e76f2eba9116d14ae7bc5f679e3
Samuel Jr. Berkoh <iamranchojr@gmail.com>
2024-06-24 02:09:35 +0000

    update: CORS configuration (#10)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import database
 from .routers import user_router, auth_router, friend_router
@@ -6,6 +7,19 @@ from .routers import user_router, auth_router, friend_router
 # fast API instance
 app = FastAPI(title='Friend Connection Backend')
 
+cors_allowed_origins = [
+    'http://localhost',
+    'http://localhost:3000',
+]
+
+# apply cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors_allowed_origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # configure routing
 app.include_router(user_router)
