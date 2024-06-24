cbbd2ee9244a3456d8c39eb48c1a1996a56e513c
Samuel Jr. Berkoh <iamranchojr@gmail.com>
2024-06-24 15:16:29 +0000

    feat: websockets integration for realtime updates (#14)

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,4 @@
 from .user_router import router as user_router
 from .auth_router import router as auth_router
 from .friend_router import router as friend_router
-
+from .websocket_router import router as websocket_router
